122a32513d57053b60cfe20c0f93eefa1f14aeb8
tahoun88-droid <tahoun_88@icloud.com>
2026-04-30 00:11:09 +0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>DM Portal 🔥</h1>
+      <p>التطبيق شغال يا معلم</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
